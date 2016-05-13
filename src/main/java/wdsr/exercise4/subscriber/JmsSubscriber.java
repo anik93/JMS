cd97c32a0503f97c41a814a424073e9530de2e9b
anik93 <proxianik@gmail.com>
2016-05-13 09:19:45 +0200

    final

--- src/main/java/wdsr/exercise4/subscriber/JmsSubscriber.java
+++ src/main/java/wdsr/exercise4/subscriber/JmsSubscriber.java
@@ -2,9 +2,6 @@ package wdsr.exercise4.subscriber;
 
 import javax.jms.Connection;
 import javax.jms.JMSException;
-import javax.jms.Message;
-import javax.jms.MessageConsumer;
-import javax.jms.MessageListener;
 import javax.jms.Session;
 import javax.jms.TextMessage;
 import javax.jms.Topic;
@@ -19,14 +16,12 @@ public class JmsSubscriber {
 	
 	private Connection connection = null;
 	private Session session = null;
-	private MessageConsumer consumer = null;
 	private TopicSubscriber topicSubscriber = null;
 	private int i = 0;
 	
 	public JmsSubscriber(final String topicName) {
 		try {
 			ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory("tcp://localhost:61616");
-			//connectionFactory.setTrustAllPackages(true);
 			connection = connectionFactory.createConnection();
 			connection.setClientID("1");
 			connection.start();
@@ -34,8 +29,6 @@ public class JmsSubscriber {
 
 			Topic topic = session.createTopic(topicName);
 
-			//consumer = session.createConsumer(topic);
-			//consumer = session.createDurableSubscriber(topic, "sub");
 			topicSubscriber = session.createDurableSubscriber(topic, "sub");
 		} catch (Exception e) {
 			log.error("Error message ", e);
@@ -59,22 +52,5 @@ public class JmsSubscriber {
 		} catch (JMSException e) {
 			log.error("Error message ", e);
 		}
-		/*MessageListener listner = new MessageListener() {
-            public void onMessage(Message message) {
-                try {
-                    if (message instanceof TextMessage) {
-                        TextMessage textMessage = (TextMessage) message;
-                        log.info(textMessage.getText());
-                    }
-                } catch (JMSException e) {
-                    log.error("error ",e);
-                }
-            }
-        };
-        try {
-			consumer.setMessageListener(listner);
-		} catch (JMSException e) {
-			log.error("error ",e);
-		}*/
 	}
 }
